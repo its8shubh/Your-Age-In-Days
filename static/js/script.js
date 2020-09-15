9eeca910c98b30b9c12d3d5ac412bddf336fa796
its8shubh <55401104+its8shubh@users.noreply.github.com>
2020-09-16 03:11:53 +0530

    Add files via upload

---
+++ static/js/script.js
@@ -0,0 +1,16 @@
+
+function ageInDays() {
+    var birthYear = prompt("What year were you born...Good friend?");
+    var ageDays = (2019-birthYear)*365;
+    var h1 = document.createElement('h1');
+    var textAnswer = document.createTextNode("You are "+ageDays+" days old");
+    h1.setAttribute('id','ageInDays');
+    h1.appendChild(textAnswer);
+    document.getElementById("flex-box-result").appendChild(h1);
+
+}
+
+function reset() {
+    document.getElementById("ageInDays").remove();
+}
+//ageInDays();
